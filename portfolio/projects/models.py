aadb8fb8e327fea675c1dc9b37c64155086b0949
filipeorfao <filipeaorfao@gmail.com>
2024-10-03 21:09:58 +0100

    change model image to image location

--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/projects/img")
+    image = models.CharField(max_length=100)
 
 
 # this is how you use orm
