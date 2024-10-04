f4b2aef69977bd113ba05ca949f548394f45640d
filipeorfao <filipeaorfao@gmail.com>
2024-10-04 17:20:52 +0100

    add detail page for projects

--- portfolio/projects/views.py
+++ portfolio/projects/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from projects.models import Project
 
@@ -11,3 +10,8 @@ def project_list(request):
 def all_projects(request):
     projects = Project.objects.all()
     return render(request, "projects/all_projects.html", {"projects": projects})
+
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, "projects/detail.html", {"project": project})
