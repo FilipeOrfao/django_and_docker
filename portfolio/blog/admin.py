6d7d2084f4e415792e45bfc38f70f54f15486cd2
filipeorfao <filipeaorfao@gmail.com>
2024-10-08 16:40:58 +0100

    change file permissinons and added magic method str to blog models

--- portfolio/blog/admin.py
+++ portfolio/blog/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 from blog.models import Category, Post, Comment
 
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register([Category, Post, Comment])
